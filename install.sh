8b8a0412c457af195f3c2633bae6e8a76641f206
Milkiway13 <160399313+Milkiway13@users.noreply.github.com>
2026-02-22 11:25:23 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo mv ./bin/* /usr/bin/
+chmod +x /usr/bin/mv-kt
+chmod +x /usr/bin/ls-kt
+chmod +x /usr/bin/chmod-kt
+chmod +x /usr/bin/cat-kt
+chmod +x /usr/bin/whoami-kt
+chmod +x /usr/bin/rm-kt
+chmod +x /usr/bin/ln-kt
+chmod +x /usr/bin/touch-kt
+chmod +x /usr/bin/cp-kt
